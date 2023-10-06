23703ebd26c584b92d8b361a858aa058684aa19a
Daniel Vera Nieto <daniel.vera-nieto@psi.ch>
2023-10-06 17:39:51 +0200

    First working version

--- reportero/main.py
+++ reportero/main.py
@@ -20,7 +20,7 @@ class Extension(enum.Enum):
 
 
 @dataclass
-class ScanStats:
+class ScanInfo:
     camera: str
     microscope: str
     objective: float
@@ -40,12 +40,11 @@ class Scan:
     created_at: datetime.datetime
     finished_at: datetime.datetime
     size: int
-
+    info: ScanInfo
 
 @dataclass
 class SimpleScan(Scan):
     data: Path
-    stats: ScanStats
 
 
 @dataclass
@@ -55,6 +54,7 @@ class StitchedScan(Scan):
     size: int = field(init=False)
     created_at: datetime.datetime = field(init=False)
     finished_at: datetime.datetime = field(init=False)
+    info: ScanInfo = field(init=False)
 
     def __post_init__(self):
         self.number_of_subscans = len(self.data)
@@ -63,33 +63,41 @@ class StitchedScan(Scan):
             0]  # First subscan sets the creation timestamp of the stitched scan
         self.finished_at = [elem.finished_at for elem in self.data][
             -1]  # Last subscan sets the finish timestamp of the stitched scan
+        self.info = self.data[0].info #The info of the stitched scan should be the same as in every subscan
+
+    def __repr__(self):
+        parent_repr = super().__repr__()
+        return parent_repr
+        # cls_annotations = self.__dict__.get('__annotations__', {})
+
+
+
 
 
 @dataclass
 class Dataset:
     path: Path
     scans: list[Scan]
-    number_of_scans: int = field(init=False)
-    size: int = field(init=False)
-    scan_time: datetime.timedelta = field(init=False)
-    efficiency: float = field(init=False)
+    number_of_scans: int = field(init=False, default=0)
+    size: tuple[float, str] = field(init=False, default=(0, 'B'))
+    scan_time: datetime.timedelta = field(init=False, default=datetime.timedelta(0))
+    efficiency: float = field(init=False, default=0.0)
 
     def __post_init__(self):
-        self.number_of_scans = len(self.scans)
-        self.size = sum([scan.size for scan in self.scans])
-        scan_times = [scan.finished_at - scan.created_at for scan in self.scans]
-        self.scan_time = sum(scan_times, datetime.timedelta())
-        self.efficiency = self.scan_time / (self.scans[-1].finished_at - self.scans[0].created_at)
-        print((self.scans[-1].finished_at - self.scans[0].created_at).total_seconds(), self.scans[-1].finished_at,
-              self.scans[0].created_at)
+        if self.scans:
+            self.number_of_scans = len(self.scans)
+            self.size = sizeof_fmt(sum([scan.size for scan in self.scans]))
+            scan_times = [scan.finished_at - scan.created_at for scan in self.scans]
+            self.scan_time = sum(scan_times, datetime.timedelta())
+            self.efficiency = self.scan_time / (self.scans[-1].finished_at - self.scans[0].created_at)
 
 
-def sizeof_fmt(num, suffix="B"):
+def sizeof_fmt(num: int, suffix: str = "B") -> tuple[float, str]:
     for unit in ("", "Ki", "Mi", "Gi", "Ti", "Pi", "Ei", "Zi"):
         if abs(num) < 1024.0:
-            return f"{num:3.1f}{unit}{suffix}"
+            return num, suffix  # f"{num:3.1f}{unit}{suffix}"
         num /= 1024.0
-    return f"{num:.1f}Yi{suffix}"
+    return num, suffix  # f"{num:.1f}Yi{suffix}"
 
 
 def find_file_by_extension(path: Path, extension: Extension) -> Union[Path, None]:
@@ -111,8 +119,10 @@ def _get_timestamps(log_file: Path) -> Union[tuple[datetime.datetime, datetime],
     # Define the patterns for matching the datetime string (thanks ChatGPT)
     start_pattern = r'scan.*started on\s+(\w{3}\s\w{3}\s\d{1,2}\s\d{2}:\d{2}:\d{2}\s\d{4})'
     # TODO: I did not manage to find a regexp matching both patterns...
-    end_pattern1 = re.compile(r'SCAN\s*FINISHED\s+at\s+(\w{3}\s\w{3}\s\d{1,2}\s\d{2}:\d{2}:\d{2}\s\d{4})', re.IGNORECASE)
-    end_pattern2 = re.compile(r'scan\s*ended\s+at\s+:\s+(\w{3}\s\w{3}\s\d{1,2}\s\d{2}:\d{2}:\d{2}\s\d{4})', re.IGNORECASE)
+    end_pattern1 = re.compile(r'SCAN\s*FINISHED\s+at\s+(\w{3}\s\w{3}\s\d{1,2}\s\d{2}:\d{2}:\d{2}\s\d{4})',
+                              re.IGNORECASE)
+    end_pattern2 = re.compile(r'scan\s*ended\s+at\s+:\s+(\w{3}\s\w{3}\s\d{1,2}\s\d{2}:\d{2}:\d{2}\s\d{4})',
+                              re.IGNORECASE)
 
     # Search for the pattern in the text
     start_match = re.search(start_pattern, text)
@@ -132,14 +142,14 @@ def _get_timestamps(log_file: Path) -> Union[tuple[datetime.datetime, datetime],
         return None, None
 
 
-def get_scan_statistics(target_file: Path) -> Union[tuple[datetime.datetime, datetime.datetime, int, ScanStats], None]:
+def get_scan_statistics(target_file: Path) -> Union[tuple[datetime.datetime, datetime.datetime, int, ScanInfo], None]:
     if target_file is None:
         return None
     stats = target_file.stat()
     size = stats.st_size
     json_file = target_file.with_suffix(suffix='.json')
     log_file = target_file.with_suffix(suffix='.log')
-    # TODO: FIx for dual_gf_edge4_cam2/m_G_double_s10_
+
     if not log_file.exists():
         logging.warning(f"Log file {log_file} was not found! Looking for log in {target_file.parent}...")
         log_file = find_file_by_extension(target_file.parent, Extension.log)
@@ -149,7 +159,7 @@ def get_scan_statistics(target_file: Path) -> Union[tuple[datetime.datetime, dat
         json_file = find_file_by_extension(target_file.parent, Extension.json)
         logging.warning(f"Using json file at {json_file}")
 
-    if log_file is None or json_file is None or "config" in json_file.name: # Avoid fallback to config file
+    if log_file is None or json_file is None or "config" in json_file.name:  # Avoid fallback to config file
         # This may occur when a scan was cancelled
         return None
 
@@ -159,29 +169,29 @@ def get_scan_statistics(target_file: Path) -> Union[tuple[datetime.datetime, dat
 
     roi = (
         log["scientificMetadata"]["detectorParameters"]["X-ROI End"] - log["scientificMetadata"]["detectorParameters"][
-            "X-ROI Start"],
+            "X-ROI Start"] + 1,
         log["scientificMetadata"]["detectorParameters"]["Y-ROI End"] - log["scientificMetadata"]["detectorParameters"][
-            "Y-ROI Start"])
+            "Y-ROI Start"] + 1)
 
-    return created_at, finished_at, size, ScanStats(camera=log["scientificMetadata"]["detectorParameters"]["Camera"],
-                                                    microscope=log["scientificMetadata"]["detectorParameters"][
+    return created_at, finished_at, size, ScanInfo(camera=log["scientificMetadata"]["detectorParameters"]["Camera"],
+                                                   microscope=log["scientificMetadata"]["detectorParameters"][
                                                         "Microscope"],
-                                                    objective=log["scientificMetadata"]["detectorParameters"][
+                                                   objective=log["scientificMetadata"]["detectorParameters"][
                                                         "Objective"],
-                                                    scintillator=log["scientificMetadata"]["detectorParameters"][
+                                                   scintillator=log["scientificMetadata"]["detectorParameters"][
                                                         "Scintillator"],
-                                                    exposure_time=log["scientificMetadata"]["detectorParameters"][
+                                                   exposure_time=log["scientificMetadata"]["detectorParameters"][
                                                         "Exposure time"], effective_pixel_size=
                                                     log["scientificMetadata"]["detectorParameters"][
                                                         "Actual pixel size"],
-                                                    number_of_projections=log["scientificMetadata"]["scanParameters"][
+                                                   number_of_projections=log["scientificMetadata"]["scanParameters"][
                                                         "Number of projections"],
-                                                    number_of_darks=log["scientificMetadata"]["scanParameters"][
+                                                   number_of_darks=log["scientificMetadata"]["scanParameters"][
                                                         "Number of darks"],
-                                                    number_of_whites=log["scientificMetadata"]["scanParameters"][
+                                                   number_of_whites=log["scientificMetadata"]["scanParameters"][
                                                         "Number of flats"], region_of_interest=roi,
 
-                                                    )
+                                                   )
 
 
 def is_stitched_scan(dataset: Path) -> bool:
@@ -202,6 +212,8 @@ def list_scans(path: Path, extension: Extension = Extension.txt, _reference_file
         target_file = find_file_by_extension(dataset, extension)
         if is_stitched_scan(dataset):
             sub_scans = list_scans(path=dataset, extension=extension, _reference_file=target_file)
+            if not sub_scans:
+                continue
             scan = StitchedScan(path=dataset, reference_file=target_file, data=sub_scans)
             scans.append(scan)
 
@@ -212,7 +224,7 @@ def list_scans(path: Path, extension: Extension = Extension.txt, _reference_file
             created_at, finished_at, size, scan_stats = stats
             _reference_file = _reference_file if _reference_file is not None else target_file
             scan = SimpleScan(path=dataset, reference_file=_reference_file, data=target_file, size=size,
-                              created_at=created_at, finished_at=finished_at, stats=scan_stats)
+                              created_at=created_at, finished_at=finished_at, info=scan_stats)
             scans.append(scan)
 
     # Sort by creation date
@@ -248,3 +260,8 @@ if __name__ == "__main__":
     path = Path(args.path).resolve()
     dataset = Dataset(path=path, scans=list_scans(path, extension=Extension[args.extension]))
     print(json.dumps(dataset, cls=EnhancedJSONEncoder, indent=4))
+    # cls_annotations = Dataset.__dict__.get('__annotations__', {})
+    # print(dataset)
+    # print(dataset.__str__())
+    # print(dataset.__repr__())
+
